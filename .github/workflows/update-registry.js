b9c354c05d14830bd575ce810b55aff2d6dfa65e
Lemmy <studio@quadbyte.net>
2026-01-25 13:50:26 -0500

    Add 'official' field to registry update

--- .github/workflows/update-registry.js
+++ .github/workflows/update-registry.js
@@ -63,6 +63,7 @@ function extractRegistryEntry(manifest, dirPath) {
     id: manifest.id,
     name: manifest.name,
     version: manifest.version,
+    official: manifest.official,
     author: manifest.author,
     description: manifest.description,
     repository: manifest.repository,
